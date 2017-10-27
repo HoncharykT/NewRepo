5eb85ef10665cc7de0b6e9ce3eb2fd09f4aefd83
DyeablTorkwe <31615468+DyeablTorkwe@users.noreply.github.com>
2017-10-27 14:28:38 +0300

    add two files

---
+++ Main.java
@@ -0,0 +1,13 @@
+package Zav2;
+
+public class VectorMain {
+
+	public static void main(String[] args) {
+		Vector v1=new Vector(1,2,3);
+		Vector v2=new Vector(2,1,-2);
+		System.out.print("Vector v1:");
+		v1.print();
+		System.out.print("Vector v2:");
+		v2.print();
+	}
+}
